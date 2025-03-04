23e263cc143e70fa7b809b7b6dcf965fa1e901d2
chengx <chengx@morgan-aps.com>
2025-03-04 02:03:41 -0600

    update p1_2.c
    separate the first part of program which uses statically allocated
    variable to p1_1.c

--- cpu-api/p1_2.c
+++ cpu-api/p1_2.c
@@ -3,32 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-//#define static
 
-#ifdef static
-int main(int argc, char *argv[]) {
-  int num = 10;
-  int *x = &num;
-  printf("Parent: the value of x is %d, (pid: %d)\n",
-         *x, (int) getpid());
-
-  int rc = fork();
-  if (rc < 0) {
-    printf("fork failed :(\n");
-    exit(1);
-  } else if (rc == 0) {
-    *x = *x+1;
-    printf("Child: the value of x is %d, (pid: %d)\n",
-           *x, (int) getpid());
-  } else {
-    wait(NULL);
-    *x = *x+2;
-    printf("Parent: the value of x is %d, (pid: %d)\n",
-           *x, (int) getpid());
-  }
-  return 0;
-}
-#else
 int main(int argc, char *argv[]) {
   int *x = (int*) malloc(sizeof(int));
   *x = 100;
@@ -48,7 +23,6 @@ int main(int argc, char *argv[]) {
     *x = *x+1;
     printf("Child: the value of x is %d at address %p, (pid: %d)\n",
            *x, x, (int) getpid());
-    sleep(1);
   } else {
     wait(NULL);
     *x = *x+2;
@@ -58,4 +32,3 @@ int main(int argc, char *argv[]) {
   free(x);
   return 0;
 }
-#endif
